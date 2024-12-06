981255a6d76a6be73da075171bcaf92e6578b95a
Deea22 <andreea.vitu@cniasi.ro>
2024-12-06 23:05:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openacasa() {
+    window.location.href="index.html";
+}
+function openinformatii(){
+    window.location.href="informatii.html";
+}
+function openbeneficii(){
+    window.location.href="beneficii.html";
+}
